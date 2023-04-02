db780d432a48d481a7891c4ace457ea0d2c05ab7
pvico <pvico@uwaterloo.ca>
2023-04-02 18:42:41 -0400

    added params and methods to block.h/cc

--- block.cc
+++ block.cc
@@ -2,7 +2,7 @@
 
 const std::string EMPTY_LINE{7, ' '};
 
-Block::Block(std::string name) : name{name} {
+Block::Block(std::string name) : name{name}, canOwn{ownable}, owner{owner} {
   for (int i = 0; i < displayLength; i++) {
     displayName[i] = EMPTY_LINE;
   }
@@ -10,6 +10,10 @@ Block::Block(std::string name) : name{name} {
 
 std::string Block::getName() {
   return name;
+}
+
+std::string Block::getOwner() {
+  return owner;
 }
 
 std::string *Block::getDisplayName() {
--- block.h
+++ block.h
@@ -7,13 +7,17 @@ extern const std::string EMPTY_LINE;
 
 class Block {
  protected:
+  bool occupiable;
   std::string name;
+  bool canOwn;
+  std::string ownedBy;
   static const int displayLength = 5;
   std::string displayName[displayLength];  // For visual display
 
  public:
-  Block(std::string name);
+  Block(std::string name, bool ownable, std::string owner);
   std::string getName();
+  std::string getOwner();
   std::string *getDisplayName();
 };
 
