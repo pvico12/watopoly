c0dd22eb47eb920decb2f7f879f7db076d25b35e
Jeffrey Gu <jgu@canarytechnologies.com>
2023-04-03 17:48:08 -0400

    Updated player.cc/.h

--- player.h
+++ player.h
@@ -3,27 +3,29 @@
 
 #include <string>
 #include <vector>
-#include "property.h"
 
-// forward declaration of property
-// may change it to include
-// class Property;
+#include "Property/academic.h"
+#include "Property/property.h"
+
+extern const int BOARD_SIZE;
 
 class Player {
   std::string name;
-  int total;
+  int position, money;
   std::vector<Property> properties;
 
  public:
+  Player(std::string name, int position = 0, int money = 0, std::vector<Property> properties = {});
+
   void move(int n);  // may put it in board instead of here
 
   void add(int amount);
 
-  bool remove(int amount); // may change to void
+  bool remove(int amount);  // may change to void
 
   bool buy(Property &prop);
 
-  bool improve(Property &prop);
+  bool improve(Academic &prop);
 
   bool trade(Player &p);  // may need to add additional parameters
 };
