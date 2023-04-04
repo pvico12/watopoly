e8c88881770da82f577a9f0d0af9b4d8f087c74b
Jeffrey Gu <jgu@canarytechnologies.com>
2023-04-04 17:37:15 -0400

    Temporary push (vs code errors), will fix later

--- NonProperty/nonproperty.h
+++ NonProperty/nonproperty.h
@@ -2,10 +2,15 @@
 #define NONPROPERTY_H
 
 #include <string>
+
 #include "../block.h"
+#include "../player.h"
 
 class NonProperty : public Block {
   // Decorator Pattern
+ public:
+  NonProperty(std::string name);
+  virtual void action(Player &player) = 0;
 };
 
 #endif
