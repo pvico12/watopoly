55c001c9e887a58649b02314dc2eb33c39a29991
Jeffrey Gu <jgu@canarytechnologies.com>
2023-04-04 17:25:53 -0400

    Temporary push (vs code errors), will fix later

--- NonProperty/MovementBlock.h
+++ NonProperty/MovementBlock.h
@@ -2,9 +2,20 @@
 #define MOVEMENTBLOCK_H
 
 #include "nonproperty.h"
+#include "player.h"
+
+enum MoveType {
+  MOVE_N_STEPS,
+  TO_BLOCK
+};
 
 class MovementBlock : public NonProperty {
-  
+  int steps;
+  MoveType mt;
+
+ public:
+  MovementBlock(std::string name, int steps, MoveType mt = MOVE_N_STEPS);
+  virtual void action(Player &player);
 };
 
 #endif
---
+++ NonProperty/nonproperty.cc
@@ -0,0 +1,4 @@
+#include "nonproperty.h"
+
+NonProperty::NonProperty(std::string name)
+    : Block{name} {}
