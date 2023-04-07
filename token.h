db5229ef8bacb0e2be1a9a482f83d91bbff2ea02
Jeffrey Gu <jgu@canarytechnologies.com>
2023-04-06 21:56:52 -0400

    Added strtotoken & tokentostr

--- token.h
+++ token.h
@@ -37,4 +37,26 @@ std::map<Token, char> tokenToCharMap = {
   {Token::TIM_HORTONS, 'T'}
 };
 
+std::map<std::string, Token> strToTokenMap = {
+  {"Goose", Token::GOOSE},
+  {"Grt_Bus", Token::GRT_BUS},
+  {"Tim_Hortons", Token::TIM_HORTONS},
+  {"Pink_Tie", Token::PINK_TIE},
+  {"Student", Token::STUDENT},
+  {"Money", Token::MONEY},
+  {"Laptop", Token::LAPTOP},
+  {"Doughnut", Token::DOUGHNUT}
+};
+
+std::map<Token, std::string> tokenToStrMap = {
+  {Token::GOOSE, "Goose"},
+  {Token::GRT_BUS, "Grt_Bus"},
+  {Token::TIM_HORTONS, "Tim_Hortons"},
+  {Token::PINK_TIE, "Pink_Tie"},
+  {Token::STUDENT, "Student"},
+  {Token::MONEY, "Money"},
+  {Token::LAPTOP, "Laptop"},
+  {Token::TIM_HORTONS, "Doughnut"}
+};
+
 #endif
