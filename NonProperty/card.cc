865ee09587478182ebac50445bd1c2759a8a29fb
Jeffrey Gu <jgu@canarytechnologies.com>
2023-04-09 00:39:16 -0400

    Added Card (may change later)

---
+++ NonProperty/card.cc
@@ -0,0 +1,48 @@
+#include "card.h"
+
+CardBlock::CardBlock(std::string name, int numCards)
+    : NonProperty{name},
+      numCards{numCards},
+      cards(numCards),
+      cumulativeChances(std::vector<double>(numCards)) {
+  double defaultChance = 1.0 / numCards;
+  double cumulativeChance = 0.0;
+  for (int i = 0; i < numCards; i++) {
+    cards[i].chance = defaultChance;
+    cumulativeChance += defaultChance;
+    cumulativeChances[i] = cumulativeChance;
+  }
+}
+
+bool CardBlock::setCards(const std::vector<Card>& newCards) {
+  if (newCards.size() != numCards) {
+    return false;
+  }
+
+  double total = 0;
+  for (const auto& card : newCards) {
+    total += card.chance;
+  }
+
+  if (total != 1) {
+    return false;
+  }
+
+  double cumulativeChance = 0;
+  for (int i = 0; i < numCards; i++) {
+    cards[i] = newCards[i];
+    cumulativeChance += newCards[i].chance;
+    cumulativeChances[i] = cumulativeChance;
+  }
+  return true;
+}
+
+void CardBlock::action(Player& player) {
+  double randomValue = static_cast<double>(rand()) / RAND_MAX;
+  for (int i = 0; i < numCards; i++) {
+    if (randomValue <= cumulativeChances[i]) {
+      cards[i].action(player);
+      break;
+    }
+  }
+}
---
+++ NonProperty/card.h
@@ -0,0 +1,24 @@
+#ifndef CARDBLOCK_H
+#define CARDBLOCK_H
+
+#include "nonproperty.h"
+#include "../player.h"
+#include <functional>
+
+struct Card {
+  std::function<void(Player&)> action;
+  double chance;
+};
+
+class CardBlock : public NonProperty {
+  int numCards;
+  std::vector<Card> cards;
+  std::vector<double> cumulativeChances;
+
+ public:
+  CardBlock(std::string name, int totalCards);
+  bool setCards(const std::vector<Card>& newCards);
+  virtual void action(Player &player);
+};
+
+#endif
